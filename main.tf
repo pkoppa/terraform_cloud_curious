d38ec2496038dda0309943cc604790a2bd54a8ba
Paniraj Koppa <paniraj.km@gmail.com>
2021-04-21 08:32:46 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,24 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "3.31.0"
     }
   }
 }
 
+
+provider "aws" {
+  region     = "us-west-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
 resource "aws_instance" "web" {
   ami           = "ami-031b673f443c2172c"
   instance_type = "t3.micro"
+
+  tags = {
+    Name = "tf-example"
+  }
 }
